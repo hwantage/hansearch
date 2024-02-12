1f5fd3c64bca9d8c2f9f76daf24063c235dd7225
james.kim <hwantagexsw2@gmail.com>
2024-02-12 22:37:18 +0900

    options 지원 및 중간 한글 문자 분해 조립 기능 추가

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,24 +3,25 @@
  * @param {Record<string, any>[]} jsonObj - 검색할 JSON 객체 배열
  * @param {string} keyWord - 초성 검색에 사용될 키워드
  * @param {Object} [options] - 옵션 객체
- * @param {string[]} [options.keys] - 검색할 특정 키(들)를 선택할 수 있음
- * @returns {Record<string, any>[]} - 검색 결과를 담은 JSON 객체 배열
+ * @param {Options string[]} [options.keys] - 검색할 특정 키(들)를 선택할 수 있습니다. 문자열 배열로 전달합니다.
+ * @param {Options Record<string, any>[]} [options.keys] - 검색 모드 지정 및 검색할 특정 키(들)를 선택할 수 있습니다. JSON 객체 배열로 전달합니다.
+ * @returns {Record<string, any>[]} - 검색 결과를 포함한 JSON 객체 배열을 반환합니다.
  * @example
  * const result = hansearch(jsonArray, "키워드"); // jsonArray의 모든 키값을 탐색
  * const result = hansearch(jsonArray, "키워드", ["key1", "key2"]); // jsonArray의 key1 및 key2 값만 탐색
  * const result = hansearch(jsonArray, "키워드").mark(); // 결과 값 <mark></mark> 태그 치환
  * const result = hansearch(jsonArray, "키워드").mark("my-tag"); // 결과 값 <my-tag></my-tag> 태그 치환
+ * const result = hansearch(jsonArray, "키워드", {'mode':'exact'}); // 정확히 일치하는 검색을 수행
+ * const result = hansearch(jsonArray, "키워드", {'mode':'exact', 'keys' : ["key1", "key2"]}); // 정확히 일치하는 검색을 수행하며, key1 및 key2 값만 탐색
  **/
 interface MarkedResult {
-    mark: (tag?: string) => Record<string, any>[];
+  mark: (tag?: string) => Record<string, any>[];
 }
 
-declare module 'hansearch' {
-    const hansearch: (
-        jsonObj: Record<string, any>[],
-        keyWord: string,
-        options?: string[]
-    ) => MarkedResult;
+type Options = Record<string, any> | string[];
 
-    export = hansearch;
+declare module "hansearch" {
+  const hansearch: (jsonObj: Record<string, any>[], keyWord: string, options?: Options) => MarkedResult;
+
+  export = hansearch;
 }
--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
   const CHO_HANGUL = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
   const JUNG_HANGUL = ["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"];
   const JONG_HANGUL = ["", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
-  const MAP_DOUBLE_JONG = { ㄳ: "ㄱㅅ", ㄵ: "ㄴㅈ", ㄶ: "ㄴㅎ", ㄺ: "ㄹㄱ", ㄻ: "ㄹㅁ", ㄼ: "ㄹㅂ", ㄽ: "ㄹㅅ", ㄾ: "ㄹㅌ", ㄿ: "ㄹㅍ", ㅀ: "ㄹㅎ", ㅄ: "ㅂㅅ" };
+  const MAP_DOUBLE_MOUM = { ㄳ: "ㄱㅅ", ㄵ: "ㄴㅈ", ㄶ: "ㄴㅎ", ㄺ: "ㄹㄱ", ㄻ: "ㄹㅁ", ㄼ: "ㄹㅂ", ㄽ: "ㄹㅅ", ㄾ: "ㄹㅌ", ㄿ: "ㄹㅍ", ㅀ: "ㄹㅎ", ㅄ: "ㅂㅅ" };
 
   const HAN_START_CHAR = "가".charCodeAt();
   const CHO_PERIOD = Math.floor("까".charCodeAt() - "가".charCodeAt());
@@ -30,7 +30,7 @@
 
   // 초성검색
   const makeRegexByCho = (keyWord = "") => {
-    const escapedSearch = keyWord.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+    const escapedSearch = keyWord.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1"); // 특수문자 검색 지원
     const regex = CHO_HANGUL.reduce((acc, cho, index) => acc.replace(new RegExp(cho, "g"), `[${combineHangul(index, 0, 0)}-${combineHangul(index + 1, 0, -1)}]`), escapedSearch);
     return new RegExp(`(${regex})`, "ig");
   };
@@ -43,8 +43,21 @@
   // 사용자의 예상 기대 결과 도출을 위해 검색 키워드를 생성. 마지막 문자를 분해 조합.
   const makeSearchWords = (keyWord = "") => {
     let keywords = [keyWord];
-    const preCharacters = keyWord.slice(0, keyWord.length - 1); // 마지막 문자를 제외한 모든 문자열
 
+    let preCharacters = keyWord.slice(0, keyWord.length - 1); // 마지막 문자를 제외한 모든 문자열
+    preCharacters = preCharacters
+      .split("")
+      .map((currentChar) => {
+        if (MAP_DOUBLE_MOUM.hasOwnProperty(currentChar)) {
+          // 앞 문자열에 겹초성만 있는 문자는 분해해서 조립
+          return MAP_DOUBLE_MOUM[currentChar];
+        } else {
+          return currentChar;
+        }
+      })
+      .join("");
+
+    //keywords.push(preCharacters);
     const lastCharacter = keyWord.charAt(keyWord.length - 1); // 마지막 문자
 
     if (lastCharacter >= "가" && lastCharacter <= "힣") {
@@ -57,11 +70,11 @@
       const jung_char = JUNG_HANGUL[jung];
       const jong_char = JONG_HANGUL[jong];
       if (jong > 0) {
-        if (MAP_DOUBLE_JONG.hasOwnProperty(jong_char)) {
+        if (MAP_DOUBLE_MOUM.hasOwnProperty(jong_char)) {
           // 겹받침인 경우
-          let result = johapHangul(cho_char, jung_char, MAP_DOUBLE_JONG[jong_char][0]);
+          let result = johapHangul(cho_char, jung_char, MAP_DOUBLE_MOUM[jong_char][0]);
           keywords.push(preCharacters + result);
-          keywords.push(preCharacters + result + MAP_DOUBLE_JONG[jong_char][1]);
+          keywords.push(preCharacters + result + MAP_DOUBLE_MOUM[jong_char][1]);
         } else {
           // 홑받침인 경우
           keywords.push(preCharacters + johapHangul(cho_char, jung_char));
@@ -70,17 +83,40 @@
       }
     } else {
       // 초성만 있는 경우
-      if (MAP_DOUBLE_JONG.hasOwnProperty(lastCharacter)) {
-        keywords.push(preCharacters + MAP_DOUBLE_JONG[lastCharacter]);
+      if (MAP_DOUBLE_MOUM.hasOwnProperty(lastCharacter)) {
+        keywords.push(preCharacters + MAP_DOUBLE_MOUM[lastCharacter]);
+      } else {
+        keywords.push(preCharacters + lastCharacter);
       }
     }
     return keywords;
   };
 
-  const hansearch = (jsonObj, keyWord, keys = []) => {
-    const searchWords = makeSearchWords(keyWord);
+  const hansearch = (jsonObj, keyWord, options) => {
+    let keys = [];
+    let searchWords = null;
+
+    // check option keys
+    if (Array.isArray(options)) {
+      keys = options; // support minor version
+    } else if (typeof options === "object" && options !== null && "keys" in options) {
+      keys = options.keys;
+    } else {
+      keys = [];
+    }
+    // check option values
+    if (typeof options === "object" && options !== null && "mode" in options) {
+      if (options.mode === "exact") {
+        searchWords = [keyWord]; // exact matching
+      } else {
+        searchWords = makeSearchWords(keyWord); // deassemble matching
+      }
+    } else {
+      searchWords = makeSearchWords(keyWord); // default deassemble matching
+    }
     const regexArray = searchWords.map((word) => makeRegexByCho(word)).filter((regex) => regex);
     //console.log(searchWords, regexArray);
+
     let searchResult = jsonObj.filter((obj) => {
       for (const key in obj) {
         if (obj.hasOwnProperty(key) && (keys.length === 0 || keys.includes(key))) {
